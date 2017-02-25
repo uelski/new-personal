d1558201564b32382b98acdaa44f242082a5fbe0
Sam Vredenburgh <sam.vburgh@gmail.com>
2017-02-25 15:22:04 -0600

    moving old express stuff to deployable app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sam Vredenburgh', subtitle: "Welcome to my website.",  description: "This is currently under construction, sorry for the mess." });
 });
 
 module.exports = router;
